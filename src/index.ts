4a71ee0c1e01358073b2a9f35fdc66ed390be728
zaryabhehe <yourlisichka15@gmail.com>
2025-08-24 15:27:24 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,31 @@ import { autoRetry } from "@grammyjs/auto-retry";
 import { parseMode } from "@grammyjs/parse-mode";
 import { run, sequentialize } from "@grammyjs/runner";
 
-import { bot } from "./config/bot";
 import { commands } from "./commands";
+import { bot } from "./config/bot";
 import { callbackQueryHandler } from "./handlers/callback-query";
 import { onBotAddedInChat } from "./handlers/on-bot-added-in-chat";
 import { onMessageHander } from "./handlers/on-message";
 import { CommandsHelper } from "./util/commands-helper";
 
-// Enable retry & parse mode
 bot.api.config.use(autoRetry());
 bot.api.config.use(parseMode("HTML"));
-
-// Ensure sequential updates per chat/user
 bot.use(
   sequentialize((ctx) => {
     return ctx.chatId?.toString() || ctx.from?.id.toString();
   }),
 );
 
-// Register all middlewares
 bot.use(commands);
 bot.use(callbackQueryHandler);
 bot.use(onMessageHander);
 bot.use(onBotAddedInChat);
 
-// Run with @grammyjs/runner
+// bot.start({
+//   onStart: () => console.log("Bot started"),
+//   drop_pending_updates: true,
+// });
 run(bot);
 
-console.log("✅ Bot started with @grammyjs/runner");
-
-// Set Telegram bot commands (helper)
+console.log("My Lord Zaryab");
 await CommandsHelper.setCommands();
